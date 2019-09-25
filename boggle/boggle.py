b460d9f9b12b5e8437b058ea878811e373d37318
Lucas Saldyt <lucassaldyt@gmail.com>
2019-09-24 23:07:09 -0700

    Fixes a few bugs

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -14,16 +14,42 @@ def load_dictionary():
 
 def create_language_trie(dictionary):
     root = TrieNode()
-    root.add('test')
-    root.add('testicles')
+    for key in dictionary:
+        root.add(key)
     return root
 
 def generate_board(n):
     return [[choice(alphabet) for _ in range(n)] for _ in range(n)]
 
+def adjacent(i, j, n):
+    return [(x, y) for x, y in [(i + 1, j), (i, j + 1), (i + 1, j + 1), (i - 1, j), (i, j - 1), (i - 1, j - 1)]
+            if x > -1 and x < n and y > -1 and y < n]
+
+def add_words(i, j, n, board, lang, words):
+    prefixes = []
+    prefixes.append((board[i][j], i, j))
+    while len(prefixes) > 0:
+        s, x, y = prefixes.pop()
+        if lang.isword(s, prefix=True):
+            if s in lang:
+                words.add(s)
+            for l, k in adjacent(x, y, n):
+                sx = s + board[l][k]
+                if lang.isword(sx, prefix=True):
+                    prefixes.append((sx, x, y))
+
 def main():
-    print(create_language_trie({}))
-    pprint(generate_board(4))
+    #print(create_language_trie(load_dictionary()))
+    lang = create_language_trie({'key'})
+    n = 4
+    #board = generate_board(4)
+    board = [list('dail'), list('isai'), list('dkey'), list('yhnr')]
+    pprint(board)
+    words = set()
+    for i in range(n):
+        for j in range(n):
+            add_words(i, j, n, board, lang, words)
+    pprint(words)
 
 if __name__ == '__main__':
     main()
--- boggle/trie.py
+++ boggle/trie.py
@@ -1,28 +1,42 @@
-
 class TrieNode:
     def __init__(self, set_radix=None):
         self.radix     = set_radix
-        self.children = set()
+        self.children  = dict()
 
     def add_character(self, c):
-        self.children.add(TrieNode(c))
+        self.children[c] = TrieNode(c)
 
     def add(self, s):
         current = self
+        s += '*'
         for c in s:
             child = TrieNode(c)
-            current.children.add(child)
+            current.children[c] = child
             current = child
 
     def all(self):
         if self.radix == None:
-            return [child.all() for child in self.children]
+            return [child.all() for child in self.children.values()]
         elif len(self.children) == 0:
             return [[self.radix]]
-        return [[self.radix] + path for child in self.children for path in child.all()]
+        return [[self.radix] + path for child in self.children.values() for path in child.all()]
 
     def __hash__(self):
         return hash(self.radix)
 
     def __str__(self):
         return '\n'.join(''.join(map(str, x)) for l in self.all() for x in l)
+
+    def isword(self, item, prefix=False):
+        current = self
+        for c in item:
+            if c in current.children:
+                current = current.children[c]
+            else:
+                return False
+        if prefix or current.radix == '*':
+            return True
+        return False
+
+    def __contains__(self, item):
+        return self.isword(item)
